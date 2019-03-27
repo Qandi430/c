0fcc62c02d93d2fd9e374fbd6646c0893b1046f2
Qandi <dltmdwo430@gmail.com>
2019-03-27 23:33:34 +0900

    chapter02 type

--- Chapter01/HelloWorld/main.c
+++ Chapter01/HelloWorld/main.c
@@ -1,4 +1,19 @@
+//전처리 문
 #include<stdio.h>
-void main() {
+
+//메인 함수
+void main() 
+{
+	//선언부
+
+	//구현부
+
+	{
+		//선언부
+
+		//구현부
+
+	}
+	//화면 출력
 	printf("Hello world!! \n");
 }
---
+++ Chapter02/Type/main.c
@@ -0,0 +1,32 @@
+//전처리문
+#include<stdio.h>
+
+//메인함수
+void main() 
+{
+	/*
+		자료형
+		- 정수형
+		  short           2byte -32,760 ~ 32,767
+		  int             4byte -2,147,483,648 ~ 2,147,483,647
+		  long            4byte -2,147,483,648 ~ 2,147,483,647
+		  unsigned short  2byte 0~65,535
+		  unsigned int    4byte 0~4,294,697,295
+		  unsigned long   4byte 0~4,294,697,295
+		
+		- 실수형
+		  float    4byte    유효자리수 : 7
+		  double   8byte    유효자리수 : 15
+
+		- 문자형
+		  char            1byte    -128 ~ 127
+		  unsiged char    1byte    0 ~ 255
+
+		  변수 선언 방법
+		  자료형 변수명;
+		  자료형 변수명 = 초기값;
+	*/
+	int a;
+	a = 1;
+	int b = 0;
+}
